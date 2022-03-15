5c9851b8ee21e451a705d35baa68b6f880b5e753
MetinAgayev <matinga@code.edu.az>
2022-03-16 01:31:04 +0400

    Views Task

---
+++ Views/Views/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Views.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index() {
+            ViewData["Name"] = "Metin";
+            return View();
+        }
+        public IActionResult About() {
+            return View();
+        }
+        public IActionResult Contact() {
+            return View();
+        }
+    }
+}
